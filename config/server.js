f12c7b15745e054b7a8b85498f74957e79ae9cb3
Aldair <aldair_fime@hotmail.com>
2022-04-25 12:31:07 -0500

    Deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ec6f2e67abc4cf0de15130639373dcd1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8512e127-5831-4fb6-876d-c4485e8c85ac'
+};
